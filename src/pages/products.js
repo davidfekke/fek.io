36c9eb61f5e60d34017e5d2c43af1febd56241ec
David Fekke <david@fekke.com>
2019-03-19 13:03:55 -0400

    Adding new products data

--- src/pages/products.js
+++ src/pages/products.js
@@ -6,14 +6,27 @@ import Footer from "../components/footer.js"
 import Article from "../components/article.js"
 import MainHelmet from "../components/mainhelmet.js"
 
+const products = [
+    { name: 'iDogYears' },
+    { name: 'iCatYears' },
+    { name: 'iLottoNum' },
+    { name: 'JaxNode' }  
+];
+
 export default () => {
     return (
         <Layout>
             <MainHelmet title="Fek.io" />
             <Navbar />
-            <Header headline="This is the Products Page" />
+            <Header headline="This is my Stuff" />
             <Article>
-                <div>Products</div>
+                <div>
+                    <h2>Apps for Download</h2>
+                    <p>Here are some of my apps you can download on Apple's iTunes App Store</p>
+                    {products.map(item => (
+                        <h2>{item.name}</h2>
+                    ))}
+                </div>
             </Article>
             <Footer />
         </Layout>
