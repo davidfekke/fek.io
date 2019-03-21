4e78f8555648b8540b8bed3920bd2ac06d9f1b97
David Fekke <david@fekke.com>
2019-03-21 12:48:09 -0400

    Adding some images to about and blog pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Header from "../components/aboutheader.js"
 import Footer from "../components/footer.js"
 import Article from "../components/article.js"
 import MainHelmet from "../components/mainhelmet.js"
+import photoOfMe from "./davidfekke.jpg"
 
 export default () => {
     return (
@@ -15,11 +16,15 @@ export default () => {
             <Article>
                 <div>
                     <h2>Need a iOS, Android or Node.js application</h2>
-                <p>This is the web site for David Fekke's mobile applications. David Fekke is a iOS developer. He also develops .NET, ColdFusion and Java web applications, with a specialization in service oriented architecture (SOA).</p>
+                    <img src={photoOfMe} alt="David Fekke" style={{ float: 'left', boxShadow: '2px 2px 5px black', width: '7rem', borderRadius: '50%', border:'1px solid orange', margin: '0.5rem' }} />
+                    <p>This is the web site for David Fekke's mobile applications. David Fekke is a iOS developer. He also develops .NET, ColdFusion and Java web applications, with a specialization in service oriented architecture (SOA).</p>
 
-                <p>If you are looking for David's Powerpoint and Keynote presentation slides, you find them at this link.</p>
+                <p>If you are looking for David's Powerpoint and presentation slides, you find them at this <a href="https://slides.com/davidfekke/" target="_blank" rel="noopener noreferrer">link</a>.</p>
 
                 <p>If you are interested in custom application development, please contact us for rates.</p>
+                <br />
+                <h3>Aviation</h3>
+                <p>David is also a Commercial pilot and a licensed remote pilot in single engine and multi engine land aircraft (ASMEL).</p>
 
                 <p>This Web site is built using Gatsby JS. </p>
                 </div>
