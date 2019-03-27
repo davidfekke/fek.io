1af1aa6bbed0854e3df4cb8abc20f553aa696815
davidfekke <david@fekke.com>
2019-03-27 08:18:17 -0400

    Adding in Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,20 @@ module.exports = {
             },
           ],
         },
-      }
+      },
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-620182-2",
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is optional
+          anonymize: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          cookieDomain: "fek.io"
+        },
+      },
     ],
   }
