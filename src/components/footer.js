f0c3662e123b57c1a0cf1affbf03de6d241c8cc2
David Fekke <david@fekke.com>
2019-03-29 10:57:50 -0400

    Improving footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Container from "./container.js"
 import { Link } from "gatsby"
-import { FaTwitter, FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
+import ExtLink from "./extlink"
+import { FaTwitter, FaFacebook, FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
+import footerStyles from "./footer.module.css"
 
 export default () => {
     return (
@@ -13,10 +15,96 @@ export default () => {
                         <a href="https://www.facebook.com/David-Fekke-LLC-178889285498948/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaFacebook size={32}  style={{ padding: '5px' }} /> </a>
                         <a href="https://www.linkedin.com/in/david-fekke-1913ba2/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaLinkedin size={32}  style={{ padding: '5px' }} /></a> 
                         <a href="https://github.com/davidfekke/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaGithub size={32}  style={{ padding: '5px' }} /></a>
+                        <a href="https://www.youtube.com/davidfekke/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaYoutube size={32}  style={{ padding: '5px' }} /></a>
                     </div>
-                    <div style={{ textAlign: 'center'}}>
-                        Copyright 2019, David Fekke.<br /> 
-                        <Link to="terms">Terms and Conditions</Link>.
+                    <nav className={footerStyles.gridarea}>
+                        <div>
+                            <ul style={{ display: 'block', 
+                            listStyle: 'none',
+                            padding: '0px' }}>
+                                <li style={{ margin: '1rem' }}>
+                                    <strong>Connect</strong>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="blog">Blog</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://twitter.com/davidfekke" name="Twitter" />
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.linkedin.com/in/david-fekke-1913ba2/" name="Linkedin" />
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.youtube.com/davidfekke/" name="Youtube" />    
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul style={{ display: 'block', 
+                            listStyle: 'none',
+                            padding: '0px' }}>
+                                <li style={{ margin: '1rem' }}>
+                                    <strong>Products</strong>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="/products/idogyears">iDogYears</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="/products/ilottonum">iLottoNum</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="/products/icatyears">iCatYears</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="/products/jaxnode">JaxNode App</Link>
+                                </li>
+                            </ul>                            
+                        </div>
+                        <div>
+                            <ul style={{ display: 'block', 
+                            listStyle: 'none',
+                            padding: '0px' }}>
+                                <li style={{ margin: '1rem' }}>
+                                    <strong>Sites</strong>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.jaxtechmeetups.com" name="Jax Tech Meetups" />
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.jaxnode.com" name="JaxNode" />
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.swyftmobile.com" name="Swyft Mobile" />
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <ExtLink uri="https://www.github.com/davidfekke" name="Github" />
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul style={{ display: 'block', 
+                            listStyle: 'none',
+                            padding: '0px'}}>
+                                <li style={{ margin: '1rem' }}>
+                                    <strong>Pages</strong>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="contact">Contact</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="about">About</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <Link to="terms">Terms and Conditions</Link>
+                                </li>
+                                <li style={{ margin: '1rem' }}>
+                                    <a href="/rss.xml">RSS Feed</a>
+                                </li>
+                            </ul>
+                        </div>    
+                    </nav>
+                    <div style={{ textAlign: 'center', fontSize: 'small'}}>
+                        Copyright 2019, David Fekke.
                     </div>
                 </div>
             </Container>
