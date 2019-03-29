c77a5ca94432025b9f3307a7279843d4f5de8c85
David Fekke <david@fekke.com>
2019-03-29 16:18:52 -0400

    Reducing the size of some of the images and making them more progressive

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,8 +9,8 @@ export default () => {
     return (
         <footer style={{ backgroundColor: '#868e96', marginTop: '2rem'}}>
             <Container>
-                <div style={{ paddingTop: '50px', paddingBottom: '50px' }}>
-                    <div style={{ textAlign: 'center', height: '3rem', paddingTop: '20px', paddingBottom: '20px',  }}>
+                <div style={{ paddingTop: '1rem', paddingBottom: '2rem' }}>
+                    <div style={{ textAlign: 'center', height: '3rem', paddingTop: '20px', paddingBottom: '10px',  }}>
                         <a href="https://twitter.com/davidfekke" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaTwitter size={32} style={{ padding: '5px' }} /></a>
                         <a href="https://www.facebook.com/David-Fekke-LLC-178889285498948/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaFacebook size={32}  style={{ padding: '5px' }} /> </a>
                         <a href="https://www.linkedin.com/in/david-fekke-1913ba2/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'black' }}><FaLinkedin size={32}  style={{ padding: '5px' }} /></a> 
@@ -95,8 +95,8 @@ export default () => {
                             </ul>
                         </div>    
                     </nav>
-                    <div style={{ textAlign: 'center', fontSize: 'small'}}>
-                        Copyright 2019, David Fekke.
+                    <div style={{ textAlign: 'center', fontSize: 'small', paddingTop: '1rem'}}>
+                        <p style={{ fontWeight: '600' }}>Copyright 2019, David Fekke</p>
                     </div>
                 </div>
             </Container>
