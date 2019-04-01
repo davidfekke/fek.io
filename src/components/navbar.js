1536a6e12da0c7b465bb53e908ea76cc4e85dc0d
davidfekke <david@fekke.com>
2019-04-01 06:31:25 -0400

    Adding fixed Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,17 +21,37 @@ const MenuItem = props => (
     </li>
 )
 
-export default () => {
-    return (<nav>
-                <Container>
-                    <ul style={{ display: 'block', 
-                                listStyle: 'none',
-                                padding: '0px' }}>
-                        <li className={styles.navbarlogo}> 
-                            <Link to="/"><img src={Logo} alt="Logo" style={{height: '22px', verticalAlign: 'middle' }} /></Link> 
-                        </li>
-                        {menuData.map(item => ( <MenuItem key={item.title} title={item.title} link={item.link} /> ))}
-                    </ul>
-                </Container>
-            </nav>)
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // if (typeof window !== 'undefined') {
+        //     window.onscroll = function () {
+        //         console.log('I am scrolling!');
+        //         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        //             document.getElementByClassName("fooboologo").style.height = "40px";
+        //         } else {
+        //             document.getElementByClassName("fooboologo").style.height = "22px";
+        //         }
+        //     }
+        // }
+    }
+
+
+    render() {
+        return (<nav className={styles.mainNavElement}>
+            <Container>
+                <ul style={{ display: 'block', 
+                            listStyle: 'none',
+                            padding: '0px' }}>
+                    <li className={styles.navbarlogo}> 
+                        <Link to="/"><img src={Logo} alt="Logo" /></Link> 
+                    </li>
+                    {menuData.map(item => ( <MenuItem key={item.title} title={item.title} link={item.link} /> ))}
+                </ul>
+            </Container>
+        </nav>)
+    }
 }
+
+export default Navbar;
