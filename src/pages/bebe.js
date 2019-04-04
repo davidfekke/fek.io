f9c95a8607ca11eb9e971d9acc7c4909bbeb5c01
David Fekke <david@fekke.com>
2019-04-04 10:02:43 -0400

    Adding my cat to her own page

---
+++ src/pages/bebe.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar.js"
+import Header from "../components/header.js"
+import Footer from "../components/footer.js"
+import Article from "../components/article.js"
+import MainHelmet from "../components/mainhelmet.js"
+import Img from "gatsby-image"
+
+
+export default ({data}) => {
+    return (
+        <Layout>
+            <MainHelmet title="Fek.io" />
+            <Navbar />
+            <Header headline="Ms Bebe Albano" />
+            <Article>
+                <h2>This is my cat Bebe</h2>
+                <Img fluid={data.file.childImageSharp.fluid} alt="My cat" />
+            </Article>
+            <Footer />
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "images/bebealbano.jpg" }) {
+      id
+      extension
+      relativePath
+      dir
+      childImageSharp {
+        fluid(maxWidth: 1160, quality: 75) {
+            ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
