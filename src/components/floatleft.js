d865ec0b5f309b56966d2ad92707b31c1d204c3f
David Fekke <david@fekke.com>
2019-04-10 16:57:43 -0400

    Add formatting to resume

---
+++ src/components/floatleft.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default ({children}) => {
+    return (
+        <div style={{ float: 'left', width: '220px' }}>
+            {children}
+        </div>
+    )
+}
+
+
