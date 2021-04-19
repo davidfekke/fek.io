896ba1368ee33fb860356beb4cf7d00511215d69
David Fekke <david@fekke.com>
2021-04-18 20:55:33 -0400

    Adding Sun-n-Fun images for 2021

---
+++ src/pages/sunandfun2021.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image";
+import Layout from "../components/layout"
+import Navbar from "../components/navbar.js"
+import Header from "../components/aboutheader.js"
+import Footer from "../components/footer.js"
+import Article from "../components/article.js"
+import MainHelmet from "../components/mainhelmet.js"
+
+const sunnfun = ({data}) => {
+    return (
+        <Layout>
+            <MainHelmet title="Fek.io" />
+            <Navbar />
+            <Header headline="Sun-n-Fun 2021" />
+            <Article>
+                <div>
+                    <h1>Sun-n-Fun 2021</h1>
+                    <p>Here are some pictures from Sun-n-Fun 2021</p>
+                    {data.sunnfunimages.edges.map(({ node }) => { 
+                        return (<div key={node.id}>
+                            <GatsbyImage image={node.childImageSharp.gatsbyImageData} alt={node.id} />
+                        </div>); 
+                    })}
+                </div>
+            </Article>
+            <Footer />
+        </Layout>
+    )
+}
+
+export const query = graphql`{
+sunnfunimages: allFile(filter: {relativeDirectory: {eq: "images/sunnfun2021"}}) {
+  edges {
+    node {
+        id
+        childImageSharp {
+          gatsbyImageData(quality: 75, layout: FULL_WIDTH, placeholder: DOMINANT_COLOR)
+        }
+      }
+    }
+  }
+}`;
+
+export default sunnfun;
