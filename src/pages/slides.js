49aa537eb3ba1d0cef82af291c1ec0578227ac38
David Fekke <david@fekke.com>
2021-06-17 16:35:34 -0400

    Removed debug code from slides.js

--- src/pages/slides.js
+++ src/pages/slides.js
@@ -9,31 +9,27 @@ import Footer from "../components/footer.js"
 import Article from "../components/article.js"
 import MainHelmet from "../components/mainhelmet.js"
 
-const Slides = ({ data }) => {
-    const files = data;
-    console.log(files);
-    return (
-        <Layout>
-            <MainHelmet title="Fek.io" />
-            <Navbar />
-            <Header headline="About FEK.IO" />
-            <Article>
-            <div>
-                <h1>Slides</h1>
-                <p>Here are copies of my slides from my presentations.</p>
-                {data.myslides.edges.map( ({node}) => {
-                    const link_path = `/${node.relativePath}`;
-                    return (<div key={node.name}>
-                            <a href={link_path}>{node.name}</a>
-                        </div>)
-                    })
-                }
-            </div>
-            </Article>
-            <Footer />
-        </Layout>
-    );
-}
+const Slides = ({ data }) => (
+    <Layout>
+        <MainHelmet title="Fek.io" />
+        <Navbar />
+        <Header headline="About FEK.IO" />
+        <Article>
+        <div>
+            <h1>Slides</h1>
+            <p>Here are copies of my slides from my presentations.</p>
+            {data.myslides.edges.map( ({node}) => {
+                const link_path = `/${node.relativePath}`;
+                return (<div key={node.name}>
+                        <a href={link_path}>{node.name}</a>
+                    </div>)
+                })
+            }
+        </div>
+        </Article>
+        <Footer />
+    </Layout>
+)
 
 export const query = graphql`{
     myslides: allFile(filter: { relativeDirectory: {eq: "slides" }}) {
