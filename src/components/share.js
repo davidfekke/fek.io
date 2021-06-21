651baef142cd756a00b37c63ee8a36b703558cef
David Fekke <david@fekke.com>
2021-06-21 16:16:42 -0400

    Remving Facebook from the share button

--- src/components/share.js
+++ src/components/share.js
@@ -2,11 +2,12 @@ import React from "react"
 import * as shareStyles from "./share.module.css"
 import { FaTwitter, FaFacebook, FaRedditAlien, FaLinkedin } from 'react-icons/fa';
 import {
-	FacebookShareButton,
     LinkedinShareButton,
 	TwitterShareButton,
 	RedditShareButton,
 } from 'react-share';
+
+/* FacebookShareButton, */
 
 // 	FaLinkedin,  LinkedinShareButton,
 
@@ -18,12 +19,12 @@ const Share = ({ socialConfig }) => (
             </span>
             <span className="text" style={{ padding: '3px' }}>Twitter</span>
         </TwitterShareButton>
-        <FacebookShareButton url={socialConfig.config.url} className={ `button is-outlined is-rounded ${shareStyles.facebook}` }  >
+        {/* <FacebookShareButton url={socialConfig.config.url} className={ `button is-outlined is-rounded ${shareStyles.facebook}` }  >
             <span className="icon" style={{ padding: '5px' }}>
                 <FaFacebook size={16} style={{ padding: '0px' }} />
             </span>
             <span className="text" style={{ padding: '3px' }}>Facebook</span>
-        </FacebookShareButton>
+        </FacebookShareButton> */}
         <LinkedinShareButton url={socialConfig.config.url} className={ `button is-outlined is-rounded ${shareStyles.linkedin}` }  title={socialConfig.config.title} >
             <span className="icon" style={{ padding: '5px' }}>
                 <FaLinkedin size={16} style={{ padding: '0px' }} />
