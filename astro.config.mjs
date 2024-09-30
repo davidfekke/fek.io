c473904bbdb476235a357d6af99c8005ba2f3906
David Fekke <david@fekke.com>
2024-09-29 21:43:39 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon()]
+});
